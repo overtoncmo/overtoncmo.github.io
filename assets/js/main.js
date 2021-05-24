26057a012e18c7975be37ec320c6a819cf36c43f
overtoncmo <overtoncmo@gmail.com>
2021-05-23 20:15:03 -0400

    modal content

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,18 @@
     "Old Fashioned":"Recipe here...."
   }
 
+  $('#recipe-modal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var drink = button.text // Extract info from data-* attributes
+  console.log(drink)
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text(drink)
+  modal.find('.modal-body input').val(recipes[drink])
+})
+
+
   /**
    * Easy selector helper function
    */
