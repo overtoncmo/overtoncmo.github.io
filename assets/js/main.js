88d2575455b9d3dfe048bbf176a3055799af2be2
overtoncmo <overtoncmo@gmail.com>
2021-05-23 20:36:21 -0400

    modal content

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@
   var button = $(event.relatedTarget) // Button that triggered the modal
   console.log("Button: ")
   console.log(button)
-  var drink = button.innerHTML // Extract info from data-* attributes
+  var drink = button.content
   console.log("Drink: ")
   console.log(drink)
 
