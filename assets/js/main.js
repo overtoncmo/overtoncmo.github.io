aeb364a3a677e9a85c5acf0ca47c1b5cf54010be
overtoncmo <overtoncmo@gmail.com>
2021-05-23 20:17:28 -0400

    modal content

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,9 @@
 
   $('#recipe-modal').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
+  console.log(button)
   var drink = button.text // Extract info from data-* attributes
-  console.log(drink)
+
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   var modal = $(this)
