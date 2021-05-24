12a1d6421da4168da03fcd1798c9cdb5d237867a
overtoncmo <overtoncmo@gmail.com>
2021-05-23 20:19:10 -0400

    modal content

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,7 @@
   var button = $(event.relatedTarget) // Button that triggered the modal
   console.log(button)
   var drink = button.text // Extract info from data-* attributes
+  console.log(drink)
 
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
